b2dff2b86a4124f9257855d5f0f0b24ca6365ab3
Deyan Dyankov <deyan@dyankov.name>
2019-12-27 21:03:29 +0000

    Exercises for Chapter 3

--- 2/2.jl
+++ 2/2.jl
@@ -1,6 +1,7 @@
 # Example from p. 36
 using LinearAlgebra
 using Plots
+using Test
 f(x) = x[1] + exp(x[2] - x[1])
 z = [1, 2]
 grad_f(z) = [1 - exp(z[2] - z[1]); exp(z[2] - z[1])]
@@ -61,3 +62,68 @@ A = [
 ]
 x = [-1, 1, 1]
 A \ x
+
+# 2.5
+A = [
+    1 0
+    1 -2
+]
+x = [1, 2]
+
+A \ x
+f(x) = [1., -.5]' * x
+f([1, -1])
+f([2, -2])
+
+# 2.6
+w1 = repeat([0, 1, 2], 15)
+w2 = repeat([0, 2, 4], 15)
+w = vcat(w1, w2)
+v = 0
+s(a) = w'*a + v
+
+@test s(vcat(repeat([0, 1, 0], 15), repeat([0, 1, 0], 15))) == 15*1 + 15*2
+@test s(vcat(repeat([0, 0, 1], 15), repeat([0, 1, 0], 15))) == 15*2 + 15*2
+@test s(vcat(repeat([0, 0, 1], 15), repeat([0, 0, 1], 15))) == 15*2 + 15*4
+
+# 2.9
+@time begin
+    f(x) = x[1] * x[2]
+    dfdx1(x) = x[2]
+    dfdx2(x) = x[1]
+    z = [1, 1]
+    f̂(x) = f(z) + dfdx1(z)*(x[1] - z[1]) + dfdx2(z)*(x[2] - z[2])
+    #
+    # or, more elegantly:
+    using Calculus
+    f̂(x) = f(z) + Calculus.gradient(f, z)'*(x - z)
+    #
+    compare_values = [
+        [1, 1],
+        [1.05, 0.95],
+        [0.85, 1.25],
+        [0.90, 1.10],
+        [0, 0],
+        [2, 2],
+        [-1, 2]
+    ]
+
+    y = map(f, compare_values)
+    ŷ = map(f̂, compare_values)
+    @show y .- ŷ
+end
+
+# 2.12
+@time begin
+    n = 10
+    Pcurr = [4.89, 1.15, 1.70, 1.52, 9.15, 1.62, 4.09, 0.76, 8.44, 6.59]
+    Pnew = [4.89, 1.15, 1.70, 1.52, 9.30, 1.64, 4.09, 0.76, 8.44, 6.59]
+    # Pnew = Pcurr
+    x = (Pnew .- Pcurr) ./ Pcurr
+    β = [0.36, 0.55, 0.9, 0.2, 1.89, 1.38, 0.78, 0.83, 0.79, 0.69]
+    P = 100 # current profit with Pcurr prices
+    P̂(x) = β'*x + P
+    @show x[5], x[6]
+    @show P, P̂(x)
+    # 1% increase in products 5 and 6 result in 4% increase in overall profits
+end
---
+++ 3/3.jl
@@ -0,0 +1,51 @@
+using LinearAlgebra
+using Distances
+using Test
+
+begin
+    u = [1.8, 2.0, -3.7, 4.7]
+    v = [0.6, 2.1, 1.9, -1.4]
+    w = [2.0, 1.9, -4.0, 4.6]
+
+    @test euclidean(u, v) ≈ norm(u - v)
+end
+
+# 3.1
+begin
+    n = 100
+    x = rand([0, 1], n)
+    y = rand([0, 1], n)
+    d = sqrt(sum((x .- y).^2))
+    @show x
+    @show y
+    @test d == euclidean(x, y)
+    @test d <= n
+end
+
+# 3.2
+begin
+    n = 10
+    m = 20
+    a = rand(n)
+    b = rand(m)
+
+    rms(x) = sqrt(x'x) / sqrt(length(x))
+    rms(a, b) = sqrt(a'a + b'b) / sqrt(length(a) + length(b))
+
+    myavg(x) = sum(x)/length(x)
+    myavg(a, b) = (sum(a) + sum(b)) / (length(a) + length(b))
+
+    x = vcat(a, b)
+
+    @test rms(x) ≈ rms(a, b)
+    @test myavg(x) ≈ myavg(a, b)
+end
+
+# 3.3
+begin
+    for i in 1:100
+        a = rand(10)
+        b = rand(10)
+        @test a'b == b'a
+    end
+end
